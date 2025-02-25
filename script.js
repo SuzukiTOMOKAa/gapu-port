a7fcf8843e79752a2b86af10d1dd605fdb8a26c6
SuzukiTOKOKAa <o9gafuka1boru@gmail.com>
2025-02-25 22:59:06 +0900

    ポートフォリオ更新

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ function displayIllustrations(data) {
   });
 
   // 拡大イベントを適用
-  console.log("拡大イベント適用");
+  console.log("拡大イベント適用1");
   addExpandEvent();
-  console.log("拡大イベント適用");
+  console.log("拡大イベント適用2");
 }
 
 // ページ読み込み時にデータを取得して表示
